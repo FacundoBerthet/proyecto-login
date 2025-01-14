19d270dc80e71fda06b81fc1d2bf9c7ad8dd1696
Facundo Berhet <f.berthet2001@gmail.com>
2025-01-14 15:09:42 -0300

    Mas verificaciones

--- script.js
+++ script.js
@@ -7,39 +7,56 @@ document.addEventListener('DOMContentLoaded', function(){
     const emailError = document.getElementById('emailError');
     const passwordError = document.getElementById('passwordError');
     const confirmPasswordError = document.getElementById('confirmPasswordError');
+    const showHideButton = document.getElementById('show-hide');
 
+    //Listeners eventos
     loginForm.addEventListener('submit', function(event){
         event.preventDefault();
-        //Por agregar: metodo que valide el formulario
+        validateForm();
     })
 
    emailInput.addEventListener('blur', function(){
-        //Por agregar: metodo que valide el mail
+        validateEmail();
    }) 
 
-   emailInput.addEventListener('change', function(){
-        //Por agregar: metodo que limpie error
+   emailInput.addEventListener('input', function(){
+        clear(emailError);
    })
 
-   passwordInput.addEventListener('change', function(){
-        //Por agregar: metodo que limpie error
+   passwordInput.addEventListener('input', function(){
+        clear(passwordError);
     })
 
-    confirmPasswordInput.addEventListener('change', function(){
-        //Por agregar: metodo que limpie error
+    confirmPasswordInput.addEventListener('input', function(){
+        clear(confirmPasswordError);
     })
+
+    //Configuracion boton mostrar/ocultar
+    showHideButton.addEventListener('click', function(){
+        const isPassword = passwordInput.type === "password";
+        passwordInput.type = isPassword ? "text" : "password";
+        confirmPasswordInput.type = isPassword ? "text" : "password";
+    });
+    
 
     //Metodo que valida el form
     function validateForm(){
+        const isValidEmail = validateEmail();
+        const isValidPassword = validatePassword();
+        const passwordMatch = validatePasswordMatch();
 
+        if(isValidEmail && isValidPassword && passwordMatch){
+            saveToLocalStorage();
+            alert("Has ingresado con exito");
+        }
     }
 
     //Metodo que valida el email
     function validateEmail(){
-        const emailRegex = /^[^\s@]+@[^\s@]+\-[^\s@]+$/;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         const emailValue = emailInput.value.trim()//trim elimina espacios vacios al comienzo y al final
         if(!emailRegex.test(emailValue)){
-            //Por agregar: mostrar error
+            showError(emailError, "Ingresa un email válido");
             return false;
         }
         return true;
@@ -48,8 +65,8 @@ document.addEventListener('DOMContentLoaded', function(){
     //Metodo que valida la contraseña
     function validatePassword(){
         const passwordValue = passwordInput.value.trim();
-        if(passwordValue.lenght < 6){
-            //por agregar: mostrar error
+        if(passwordValue.length < 6){
+            showError(passwordError, "Ingresa una contraseña de al menos 6 caracteres")
             return false;
         }
         return true
@@ -60,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function(){
         const passwordValue = passwordInput.value.trim();
         const confirmPasswordValue = confirmPasswordInput.value.trim();
         if(passwordValue != confirmPasswordValue){
-            //por agregar: mostrar error
+            showError(confirmPasswordError, "Las contraseñas no coinciden")
             return false;
         }
         return true;
@@ -76,5 +93,21 @@ document.addEventListener('DOMContentLoaded', function(){
     function clear(errorElement){
         errorElement.innerHTML = "";
         errorElement.style.display = "none";
+    }
+
+    //Metodo que guarda elementos en el localStorage
+    function saveToLocalStorage(){
+        const emailValue = emailInput.value.trim();
+        localStorage.setItem('email', emailValue);
+        const body = bodyBuilderJSON();
+        console.log(body);
+    }
+
+    //Metodo que crea un JSON
+    function bodyBuilderJSON(){
+        return {
+            "email": emailInput.value,
+            "password": passwordInput.value
+        }
     }
 })
